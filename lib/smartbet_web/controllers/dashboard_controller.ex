48adac08f4eeed7610ab93582f9a80cb73d091a3
Alex Miramontes <alexmiramontes15@gmail.com>
2021-07-27 17:59:35 -0600

    Change Layouts

--- lib/smartbet_web/controllers/dashboard_controller.ex
+++ lib/smartbet_web/controllers/dashboard_controller.ex
@@ -1,5 +1,7 @@
 defmodule SmartbetWeb.DashboardController do
     use SmartbetWeb, :controller
+
+    plug :put_layout, "dashboard_layout.html"
   
     def index(conn, _params) do
       render(conn, "index.html")
