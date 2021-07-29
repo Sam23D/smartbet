6eb4d05d8a12c71cfd4dfbc09a0888dedd5b6360
Alex Miramontes <alexmiramontes15@gmail.com>
2021-07-28 21:53:16 -0600

    Change User logout

--- lib/smartbet_web/router.ex
+++ lib/smartbet_web/router.ex
@@ -75,7 +75,7 @@ defmodule SmartbetWeb.Router do
   scope "/", SmartbetWeb do
     pipe_through [:browser]
 
-    delete "/users/log_out", UserSessionController, :delete
+    get "/users/log_out", UserSessionController, :delete
     get "/users/confirm", UserConfirmationController, :new
     post "/users/confirm", UserConfirmationController, :create
     get "/users/confirm/:token", UserConfirmationController, :confirm
