781f199b91176ee21782a438fba36361b946f50d
Alex Miramontes <alexmiramontes15@gmail.com>
2021-07-28 22:02:13 -0600

    delete old login and signup

--- lib/smartbet_web/controllers/page_controller.ex
+++ lib/smartbet_web/controllers/page_controller.ex
@@ -7,12 +7,4 @@ defmodule SmartbetWeb.PageController do
     render(conn, "index.html")
   end
 
-  def login(conn, _params) do
-    render(conn, "login.html")
-  end
-
-  def signup(conn, _params) do
-    render(conn, "signup.html")
-  end
-
 end
