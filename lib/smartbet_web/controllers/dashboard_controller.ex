35a976e3e0967162aeaff1a8f3f63cd98afc92da
Samuel Blanco <dev.sam23d@gmail.com>
2021-08-29 19:47:30 -0600

    Fixes navbar dashboard

--- lib/smartbet_web/controllers/dashboard_controller.ex
+++ lib/smartbet_web/controllers/dashboard_controller.ex
@@ -1,10 +1,13 @@
 defmodule SmartbetWeb.DashboardController do
     use SmartbetWeb, :controller
 
+    alias Smartbet.Bets.UserBets
+
     plug :put_layout, "dashboard_layout.html"
 
     def index(conn, _params) do
-      render(conn, "index.html")
+      changeset = UserBets.changeset(%UserBets{}, %{})
+      render(conn, "index.html", changeset: changeset)
     end
 
     def scores(conn, _params) do
