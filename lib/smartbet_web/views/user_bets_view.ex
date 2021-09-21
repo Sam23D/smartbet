b82db49c711c19366169db730c7e975609ecbdc3
Alex Miramontes <alexmiramontes15@gmail.com>
2021-09-21 17:37:51 -0600

    Fixes bet result class

--- lib/smartbet_web/views/user_bets_view.ex
+++ lib/smartbet_web/views/user_bets_view.ex
@@ -5,4 +5,24 @@ defmodule SmartbetWeb.UserBetsView do
     date
     |> Timex.format!("{D}/{M}/{YYYY}")
   end
+
+  def result_class(result) do
+
+    case result do
+       "Pending" ->
+        "is-warning is-rounded tag"
+
+        "Win" ->
+          "is-success is-rounded tag"
+
+        "Lost" ->
+          "is-danger is-rounded tag"
+
+          _ ->
+            "is-danger is-rounded tag"
+
+    end
+
+  end
+
 end
