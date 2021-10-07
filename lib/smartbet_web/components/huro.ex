8fae8c5af169d5885ddf4c1ee96610767ad19102
Samuel Blanco <dev.sam23d@gmail.com>
2021-10-07 17:51:56 -0600

    Adds pagination component

--- lib/smartbet_web/components/huro.ex
+++ lib/smartbet_web/components/huro.ex
@@ -7,6 +7,15 @@ defmodule SmartbetWeb.Components.Huro do
     """
   end
 
+  #def pagination(%{ page_number: pn, page_size: ps, total_entries: te, total_pages: tp }) do
+  def pagination(assigns) do
+    ~H"""
+    pagination, total: <%= @user_bets.page_number %>
+    page_size: <%= @user_bets.page_size %>
+    total_entries: <%= @user_bets.total_entries %>
+    total_pages: <%= @user_bets.total_pages %>
+    """
+  end
 
 
 end
