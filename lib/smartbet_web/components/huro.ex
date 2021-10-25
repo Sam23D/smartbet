cda71999807009eed2925e662143dbec4d217590
Alex Miramontes <alexmiramontes15@gmail.com>
2021-10-25 11:18:36 -0600

    Merge branch 'net_profit_calculation' of https://github.com/Sam23D/smartbet into net_profit_calculation

--- lib/smartbet_web/components/huro.ex
+++ lib/smartbet_web/components/huro.ex
@@ -8,58 +8,69 @@ defmodule SmartbetWeb.Components.Huro do
   end
 
   #def pagination(%{ page_number: pn, page_size: ps, total_entries: te, total_pages: tp }) do
-  def pagination(assigns) do
-    _pagination(assigns.user_bets, assigns)
+  def pagination(%{page: page}=assigns) do
+    _pagination(page, assigns)
   end
 
-  defp _pagination(%{page_number: pn, total_pages: tp}, assigns) when pn == tp  do
-    prev =  "?page=#{pn - 1}"
-    final = "?page=#{tp}"
+  defp _pagination_hrefs(%{page_number: pn, total_pages: tp})do
+    %{ page_number: "?page=#{pn}", final_page: "?page=#{tp}", prev_page: "?page=#{pn - 1}", next_page: "?page=#{pn + 1}" }
+  end
 
+  defp _pagination(%{page_number: pn, total_pages: tp}=page, assigns) when pn == tp and tp == 1  do
+    %{ }  = _pagination_hrefs(page)
     ~H"""
     <nav class="flex-pagination pagination is-rounded" aria-label="pagination" data-filter-hide>
-      <a href={prev} class="pagination-previous has-chevron"><i data-feather="chevron-left"></i></a>
+      <ul class="pagination-list">
+        <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page"><%= @page.page_number %></a>
+        </li>
+      </ul>
+    </nav>
+    """
+  end
+
+  defp _pagination(%{page_number: pn, total_pages: tp}=page, assigns) when pn == tp  do
+    %{prev_page: prev_href, final_page: final_href }  = _pagination_hrefs(page)
+    ~H"""
+    <nav class="flex-pagination pagination is-rounded" aria-label="pagination" data-filter-hide>
+      <a href={prev_href} class="pagination-previous has-chevron"><i data-feather="chevron-left"></i></a>
       <ul class="pagination-list">
         <li><a href="?page=1" class="pagination-link" aria-label="Goto page 1">1</a></li>
         <li><span class="pagination-ellipsis">…</span></li>
-        <li><a href={final} class="pagination-link is-current" aria-label="Page 46" aria-current="page"><%= @user_bets.page_number %></a>
+        <li><a href={final_href} class="pagination-link is-current" aria-label="Page 46" aria-current="page"><%= @page.page_number %></a>
         </li>
       </ul>
     </nav>
     """
   end
 
-  defp _pagination(%{page_number: pn, total_pages: tp}, assigns) when pn == 1  do
-    next = "?page=#{pn + 1}"
-    final = "?page=#{tp}"
+  defp _pagination(%{page_number: pn, total_pages: tp}=page, assigns) when pn == 1  do
+    %{next_page: next_href, final_page: final_href} = _pagination_hrefs(page)
     ~H"""
     <nav class="flex-pagination pagination is-rounded" aria-label="pagination" data-filter-hide>
-      <a href={next} class="pagination-next has-chevron"><i data-feather="chevron-right"></i></a>
+      <a href={next_href} class="pagination-next has-chevron"><i data-feather="chevron-right"></i></a>
       <ul class="pagination-list">
-        <li><a  class="pagination-link is-current" aria-label="Page 46" aria-current="page"><%= @user_bets.page_number %></a>
+        <li><a  class="pagination-link is-current" aria-label="Page 46" aria-current="page"><%= @page.page_number %></a>
         </li>
         <li><span class="pagination-ellipsis">…</span></li>
-        <li><a href={final} class="pagination-link" aria-label="Goto page 86"><%= @user_bets.total_pages %></a></li>
+        <li><a href={final_href} class="pagination-link" aria-label="Goto page 86"><%= @page.total_pages %></a></li>
       </ul>
     </nav>
     """
   end
 
-  defp _pagination(%{page_number: pn, total_pages: tp}, assigns)  do
-    prev =  "?page=#{pn - 1}"
-    next = "?page=#{pn + 1}"
-    final = "?page=#{tp}"
+  defp _pagination(%{page_number: pn, total_pages: tp}=page, assigns)  do
+    %{next_page: next_href, final_page: final_href, previous_page: prev_href} = _pagination_hrefs(page)
     ~H"""
     <nav class="flex-pagination pagination is-rounded" aria-label="pagination" data-filter-hide>
-      <a href={prev} class="pagination-previous has-chevron"><i data-feather="chevron-left"></i></a>
-      <a href={next} class="pagination-next has-chevron"><i data-feather="chevron-right"></i></a>
+      <a href={prev_href} class="pagination-previous has-chevron"><i data-feather="chevron-left"></i></a>
+      <a href={next_href} class="pagination-next has-chevron"><i data-feather="chevron-right"></i></a>
       <ul class="pagination-list">
       <li><a href="?page=1" class="pagination-link" aria-label="Goto page 1">1</a></li>
         <li><span class="pagination-ellipsis">…</span></li>
-        <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page"><%= @user_bets.page_number %></a>
+        <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page"><%= @page.page_number %></a>
         </li>
         <li><span class="pagination-ellipsis">…</span></li>
-        <li><a href={final} class="pagination-link" aria-label="Goto page 86"><%= @user_bets.total_pages %></a></li>
+        <li><a href={final_href} class="pagination-link" aria-label="Goto page 86"><%= @page.total_pages %></a></li>
       </ul>
     </nav>
     """
