210558845e0ae31823a7a262d62c26929b5bd257
Samuel Blanco <dev.sam23d@gmail.com>
2021-12-04 16:47:37 -0700

    Hotfixes live console

--- lib/smartbet_web/controllers/dashboard_controller.ex
+++ lib/smartbet_web/controllers/dashboard_controller.ex
@@ -2,8 +2,9 @@ defmodule SmartbetWeb.DashboardController do
     use SmartbetWeb, :controller
 
     alias Smartbet.Bets.UserBets
+    alias Smartbet.Sports
 
-    plug :put_layout, "dashboard_layout.html"
+    # plug :put_layout, "dashboard_layout.html"
 
     def index(conn, _params) do
       changeset = UserBets.changeset(%UserBets{}, %{})
@@ -12,7 +13,8 @@ defmodule SmartbetWeb.DashboardController do
 
     def scores(conn, _params) do
       changeset = UserBets.changeset(%UserBets{}, %{})
-      render(conn, "scores.html", changeset: changeset)
+      leagues = Sports.list_tracked_basketball_leagues()
+      render(conn, "scores.html", changeset: changeset, leagues: leagues)
     end
 
 
